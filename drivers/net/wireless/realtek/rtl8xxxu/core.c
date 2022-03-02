71c8a6c13e9c81f239b9a919baa843c4d1b9a56f
Realtek nic sw <nic_swsd@realtek.com>
2022-03-02 19:41:09 +0530

    drivers: net: OEM drivers Realtek 8192EU(11n) USB 2.0 WLAN
    taken from:
    https://github.com/morrownr/88x2bu-20210702.git
    https://github.com/clnhub/rtl8192eu-linux.git
    
    modules will be named 8192eu & 88x2bu
    
    Realtek nic sw <nic_swsd@realtek.com>

--- drivers/net/wireless/realtek/rtl8xxxu/core.c
+++ drivers/net/wireless/realtek/rtl8xxxu/core.c
@@ -8020,6 +8020,7 @@ static const struct usb_device_id dev_table[] = {
 	.driver_info = (unsigned long)&rtl8723au_fops},
 {USB_DEVICE_AND_INTERFACE_INFO(USB_VENDOR_ID_REALTEK, 0x0724, 0xff, 0xff, 0xff),
 	.driver_info = (unsigned long)&rtl8723au_fops},
+#ifndef CONFIG_RTL8192EU_OEM
 {USB_DEVICE_AND_INTERFACE_INFO(USB_VENDOR_ID_REALTEK, 0x818b, 0xff, 0xff, 0xff),
 	.driver_info = (unsigned long)&rtl8192eu_fops},
 /* TP-Link TL-WN822N v4 */
@@ -8031,6 +8032,7 @@ static const struct usb_device_id dev_table[] = {
 /* Tested by Myckel Habets */
 {USB_DEVICE_AND_INTERFACE_INFO(0x2357, 0x0109, 0xff, 0xff, 0xff),
 	.driver_info = (unsigned long)&rtl8192eu_fops},
+#endif
 {USB_DEVICE_AND_INTERFACE_INFO(USB_VENDOR_ID_REALTEK, 0xb720, 0xff, 0xff, 0xff),
 	.driver_info = (unsigned long)&rtl8723bu_fops},
 {USB_DEVICE_AND_INTERFACE_INFO(0x7392, 0xa611, 0xff, 0xff, 0xff),
@@ -8250,6 +8252,7 @@ static const struct usb_device_id dev_table[] = {
 	.driver_info = (unsigned long)&rtl8192cu_fops},
 {USB_DEVICE_AND_INTERFACE_INFO(0x7392, 0x7822, 0xff, 0xff, 0xff),
 	.driver_info = (unsigned long)&rtl8192cu_fops},
+#ifndef CONFIG_RTL8192EU_OEM
 /* found in rtl8192eu vendor driver */
 {USB_DEVICE_AND_INTERFACE_INFO(0x2357, 0x0107, 0xff, 0xff, 0xff),
 	.driver_info = (unsigned long)&rtl8192eu_fops},
@@ -8269,6 +8272,7 @@ static const struct usb_device_id dev_table[] = {
 /* Mercusys MW300UH */
 {USB_DEVICE_AND_INTERFACE_INFO(0x2c4e, 0x0104, 0xff, 0xff, 0xff),
 	.driver_info = (unsigned long)&rtl8192eu_fops},
+#endif
 #endif
 { }
 };
