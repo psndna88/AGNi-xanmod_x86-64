fd1c45c8cc477fad93f0efbce1be73073ac07764
psndna88 <psndna88@gmail.com>
2022-03-24 15:56:28 +0530

    AGNi-xanmod v1.4 tt 5.17.0
    Implement tested recomended firmware file for Qualcomm QCA9377 WLAN (replaces the stock firmware file) [Revison 2]
    
    ath10k_pci firmware ver CNSS.TF.1.0-00267-QCATFSWPZ-1 api 5 features ignore-otp crc32 d6b81b3c

---
+++ AGNi_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## AGNi version info
+KERNELDIR=`readlink -f .`
+
+export AGNI_VERSION="v1.4"
+export AGNI_KERNEL_LINUX="5.17.0"
+sed -i 's/agni-v1.3/agni-v1.4/' $KERNELDIR/CONFIGS/agni-xanmod/agni*config
+sed -i 's/5.17.9 Kernel/5.17.0 Kernel/' $KERNELDIR/CONFIGS/agni-xanmod/agni*config
+
+echo "	AGNi Version info loaded."
+
