da60f7fbce81d929e33fa5b26febfe4427549e65
psndna88 <psndna88@gmail.com>
2022-03-24 10:58:04 +0530

    AGNi-xanmod v1.4 tt 5.16.17

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION="v1.4"
-export AGNI_KERNEL_LINUX="5.16.16"
+export AGNI_KERNEL_LINUX="5.16.17"
 sed -i 's/agni-v1.3/agni-v1.4/' $KERNELDIR/CONFIGS/agni-xanmod/agni*config
-sed -i 's/5.16.15 Kernel/5.16.16 Kernel/' $KERNELDIR/CONFIGS/agni-xanmod/agni*config
+sed -i 's/5.16.16 Kernel/5.16.17 Kernel/' $KERNELDIR/CONFIGS/agni-xanmod/agni*config
 
 echo "	AGNi Version info loaded."
 
