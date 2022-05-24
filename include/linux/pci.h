24753a858a5726dc2af7b13e6355e8c5aaae37c6
psndna88 <psndna88@gmail.com>
2022-05-24 07:27:51 +0530

    drivers: net: OEM drivers Realtek 5.18 compat fixes
    Provide the legacy pci_dma_* API

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -2473,7 +2473,8 @@ static inline bool pci_is_thunderbolt_attached(struct pci_dev *pdev)
 void pci_uevent_ers(struct pci_dev *pdev, enum  pci_ers_result err_type);
 #endif
 
-#include <linux/dma-mapping.h>
+/* Provide the legacy pci_dma_* API */
+#include <linux/pci-dma-compat.h>
 
 #define pci_printk(level, pdev, fmt, arg...) \
 	dev_printk(level, &(pdev)->dev, fmt, ##arg)
