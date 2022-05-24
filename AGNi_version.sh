6b249f61a5a610e257230b0c5c9cca0b13b6613d
psndna88 <psndna88@gmail.com>
2022-05-24 07:45:53 +0530

    AGNi-v1.4 tt 5.18.0
    Add core2duo/quad optimisation variant: core2series
    core2series, haswell, sandybrigde, westmere set CONFIG_HZ=100
    
    Add RTL8188FU firmware
    
    Implement tested recomended firmware file for Qualcomm QCA9377 WLAN (replaces the stock firmware file) [Revison 2]
    
    ath10k_pci firmware ver CNSS.TF.1.0-00267-QCATFSWPZ-1 api 5 features ignore-otp crc32 d6b81b3c

---
+++ AGNi_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## AGNi version info
+KERNELDIR=`readlink -f .`
+
+export AGNI_VERSION="v1.4"
+export AGNI_KERNEL_LINUX="5.18.0"
+sed -i 's/agni-v1.4/agni-v1.4/' $KERNELDIR/CONFIGS/agni/agni*config
+sed -i 's/5.18.0 Kernel/5.18.0 Kernel/' $KERNELDIR/CONFIGS/agni/agni*config
+
+echo "	AGNi Version info loaded."
+
