424759e35e6564a6bf995b2e39ae3ce63774c77f
psndna88 <psndna88@gmail.com>
2022-06-16 12:09:45 +0530

    AGNi-v1.6 tt 5.18.4

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,10 +3,10 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION="v1.5"
-export AGNI_KERNEL_LINUX="5.18.3"
-sed -i 's/agni-v1.4/agni-v1.5/' $KERNELDIR/CONFIGS/agni/agni*config
-sed -i 's/5.18.2 Kernel/5.18.3 Kernel/' $KERNELDIR/CONFIGS/agni/agni*config
+export AGNI_VERSION="v1.6"
+export AGNI_KERNEL_LINUX="5.18.4"
+sed -i 's/agni-v1.5/agni-v1.6/' $KERNELDIR/CONFIGS/agni/agni*config
+sed -i 's/5.18.3 Kernel/5.18.4 Kernel/' $KERNELDIR/CONFIGS/agni/agni*config
 
 echo "	AGNi Version info loaded."
 
