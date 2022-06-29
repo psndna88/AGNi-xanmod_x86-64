23e0ee9a6c4445f1347ff3de260347064a550d28
psndna88 <psndna88@gmail.com>
2022-06-29 20:30:24 +0530

    AGNi-v1.7 tt 5.18.8
    disable some debugging & AUDIT support

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION="v1.6"
-export AGNI_KERNEL_LINUX="5.18.6"
-sed -i 's/agni-v1.5/agni-v1.6/' $KERNELDIR/CONFIGS/agni/agni*config
-sed -i 's/5.18.4 Kernel/5.18.6 Kernel/' $KERNELDIR/CONFIGS/agni/agni*config
+export AGNI_KERNEL_LINUX="5.18.8"
+sed -i 's/agni-v1.6/agni-v1.7/' $KERNELDIR/CONFIGS/agni/agni*config
+sed -i 's/5.18.8 Kernel/5.18.8 Kernel/' $KERNELDIR/CONFIGS/agni/agni*config
 
 echo "	AGNi Version info loaded."
 
