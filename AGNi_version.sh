fd9d71a84a6c0bfb1858d259b0b393783c820fdf
psndna88 <psndna88@gmail.com>
2022-07-01 12:35:32 +0530

    AGNi-v1.8 tt 5.18.8 (stop optimized builds for haswell,sandybridge,skylake,westmere)
    disable CONFIG_DEBUG_KERNEL
    selectively enable CONFIG_DEBUG_FS
    
    intention to further reduce overhead with minimal compat breakage for production users who never need to debug kernel code.

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,9 +3,9 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION="v1.6"
+export AGNI_VERSION="v1.8"
 export AGNI_KERNEL_LINUX="5.18.8"
-sed -i 's/agni-v1.6/agni-v1.7/' $KERNELDIR/CONFIGS/agni/agni*config
+sed -i 's/agni-v1.7/agni-v1.8/' $KERNELDIR/CONFIGS/agni/agni*config
 sed -i 's/5.18.8 Kernel/5.18.8 Kernel/' $KERNELDIR/CONFIGS/agni/agni*config
 
 echo "	AGNi Version info loaded."
--- config_sync.sh
+++ config_sync.sh
@@ -4,11 +4,7 @@ export KERNELDIR=`readlink -f .`
 echo " "
 echo " 1: generic       	x86-64 build"
 echo " 2: AMD_zen2      	x86-64 build"
-echo " 3: Intel_skylake 	x86-64 build"
-echo " 4: Intel_sandybridge 	x86-64 build"
-echo " 5: Intel_westmere 	x86-64 build"
-echo " 6: Intel_haswell 	x86-64 build"
-echo " 7: Intel_core2series 	x86-64 build"
+echo " 3: Intel_core2series 	x86-64 build"
 echo " "
 echo " 0:  X  Exit Compilation  X"
 echo " "
@@ -21,18 +17,6 @@ elif [ $choice -eq 2 ]; then
 	CONFIG="agni_zen2_config"
 	TYPE="agni amd-zen2"
 elif [ $choice -eq 3 ]; then
-	CONFIG="agni_skylake_config"
-	TYPE="agni intel-skylake"
-elif [ $choice -eq 4 ]; then
-	CONFIG="agni_sandybridge_config"
-	TYPE="agni intel-sandybridge"
-elif [ $choice -eq 5 ]; then
-	CONFIG="agni_westmere_config"
-	TYPE="agni intel-westmere"
-elif [ $choice -eq 6 ]; then
-	CONFIG="agni_haswell_config"
-	TYPE="agni intel-haswell"
-elif [ $choice -eq 7 ]; then
 	CONFIG="agni_core2series_config"
 	TYPE="agni intel-core2series"
 elif [ $choice -eq 0 ]; then
