8821abc0bd8f261dc3385756a7eafd1b7e86e2e0
Realtek nic sw <nic_swsd@realtek.com>
2023-03-20 10:25:44 +0530

    drivers: net: (UPDATE) OEM drivers Realtek 8822BU(11ac) USB 2.0/3.0 WLAN
    taken from:
    https://github.com/morrownr/88x2bu-20210702.git
    
    modules will be named 88x2bu
    
    Realtek nic sw <nic_swsd@realtek.com>

--- drivers/net/wireless/realtek/rtl88x2bu/core/rtw_recv.c
+++ drivers/net/wireless/realtek/rtl88x2bu/core/rtw_recv.c
@@ -4068,9 +4068,8 @@ int recv_frame_monitor(_adapter *padapter, union recv_frame *rframe)
 
 	/* read skb information from recv frame */
 	pskb = rframe->u.hdr.pkt;
-	pskb->head = rframe->u.hdr.rx_head;
-	pskb->data = rframe->u.hdr.rx_data;
 	pskb->len = rframe->u.hdr.len;
+	pskb->data = rframe->u.hdr.rx_data;
 	skb_set_tail_pointer(pskb, rframe->u.hdr.len);
 
 	if (ndev->type == ARPHRD_IEEE80211_RADIOTAP) {
@@ -4754,8 +4753,10 @@ u8 adapter_allow_bmc_data_rx(_adapter *adapter)
 	if (MLME_IS_AP(adapter))
 		return 0;
 
+#if !defined(RHEL8)
 	if (rtw_linked_check(adapter) == _FALSE)
 		return 0;
+#endif
 
 	return 1;
 }
