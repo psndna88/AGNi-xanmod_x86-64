3065a2b63208b1797a1200a94ab8fae8f2f3fc43
Parvinder Singh <psndna88@gmail.com>
2025-05-27 13:02:11 +0530

    AGNi-v6.1 [6.12.30]

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,10 +3,10 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION="v6.0"
-export AGNI_KERNEL_LINUX="6.12.29"
-sed -i 's/agni-v5.9/agni-v6.0/' $KERNELDIR/CONFIGS/agni/agni*config
-sed -i 's/6.12.28 Kernel/6.12.29 Kernel/' $KERNELDIR/CONFIGS/agni/agni*config
+export AGNI_VERSION="v6.1"
+export AGNI_KERNEL_LINUX="6.12.30"
+sed -i 's/agni-v6.0/agni-v6.1/' $KERNELDIR/CONFIGS/agni/agni*config
+sed -i 's/6.12.29 Kernel/6.12.30 Kernel/' $KERNELDIR/CONFIGS/agni/agni*config
 
 echo "	AGNi Version info loaded."
 
