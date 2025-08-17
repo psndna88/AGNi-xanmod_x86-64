8da2504857a5356c4cb36a39b7257c3bb9ad3641
Parvinder Singh <psndna88@gmail.com>
2025-08-17 18:13:00 +0530

    AGNi-v7.2.1 [6.16.1]

---
+++ config_sync.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+export KERNELDIR=`readlink -f .`
+
+echo " "
+echo " 1: generic       	x86-64 build"
+echo " 2: amd zen2       	x86-64 build"
+echo " 3: intel ivybridge     	x86-64 build"
+echo " 4: intel westmere       	x86-64 build"
+echo " 5: intel broadwell      	x86-64 build"
+echo " 6: intel alderlake      	x86-64 build"
+echo " 0:  X  Exit Compilation  X"
+echo " "
+read -p "    Select type of config : " choice
+
+if [ $choice -eq 1 ]; then
+	CONFIG="agni_generic_config"
+	TYPE="agni generic"
+elif [ $choice -eq 2 ]; then
+	CONFIG="agni_zen2_config"
+	TYPE="agni amd zen2"
+elif [ $choice -eq 3 ]; then
+	CONFIG="agni_ivybridge_config"
+	TYPE="agni intel ivybridge"
+elif [ $choice -eq 4 ]; then
+	CONFIG="agni_westmere_config"
+	TYPE="agni intel westmere"
+elif [ $choice -eq 5 ]; then
+	CONFIG="agni_broadwell_config"
+	TYPE="agni intel broadwell"
+elif [ $choice -eq 6 ]; then
+	CONFIG="agni_alderlake_config"
+	TYPE="agni intel alderlake"
+elif [ $choice -eq 0 ]; then
+	exit
+else
+	echo " "
+	echo -e "====> Enter corrent input <===="
+fi
+
+cp -f $KERNELDIR/.config $KERNELDIR/CONFIGS/agni/$CONFIG
+echo "   configs saved as $TYPE!"
+
