79ee82759b52182aea75f82c9af5b5c2f813817c
Parvinder Singh <psndna88@gmail.com>
2026-03-22 12:59:03 +0530

    AGNi-v7.6.7 [6.18.29]

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,10 +3,10 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION="v7.6.6"
-export AGNI_KERNEL_LINUX="6.18.18"
-sed -i 's/agni-v7.6.5/agni-v7.6.6/' $KERNELDIR/CONFIGS/agni/agni*config
-sed -i 's/6.18.16 Kernel/6.18.18 Kernel/' $KERNELDIR/CONFIGS/agni/agni*config
+export AGNI_VERSION="v7.6.7"
+export AGNI_KERNEL_LINUX="6.18.29"
+sed -i 's/agni-v7.6.6/agni-v7.6.7/' $KERNELDIR/CONFIGS/agni/agni*config
+sed -i 's/6.18.16 Kernel/6.18.29 Kernel/' $KERNELDIR/CONFIGS/agni/agni*config
 
 echo "	AGNi Version info loaded."
 
--- config_sync.sh
+++ config_sync.sh
@@ -4,10 +4,8 @@ export KERNELDIR=`readlink -f .`
 echo " "
 echo " 1: generic       	x86-64 build"
 echo " 2: intel ivybridge     	x86-64 build"
-echo " 3: intel westmere       	x86-64 build"
-echo " 4: intel broadwell      	x86-64 build"
-echo " 5: intel haswell         x86-64 build"
-echo " 6: intel alderlake      	x86-64 build"
+echo " 3: intel haswell         x86-64 build"
+echo " 4: intel alderlake      	x86-64 build"
 echo " 0:  X  Exit Compilation  X"
 echo " "
 read -p "    Select type of config : " choice
@@ -19,15 +17,9 @@ elif [ $choice -eq 2 ]; then
 	CONFIG="agni_ivybridge_config"
 	TYPE="agni intel ivybridge"
 elif [ $choice -eq 3 ]; then
-	CONFIG="agni_westmere_config"
-	TYPE="agni intel westmere"
-elif [ $choice -eq 4 ]; then
-	CONFIG="agni_broadwell_config"
-	TYPE="agni intel broadwell"
-elif [ $choice -eq 5 ]; then
 	CONFIG="agni_haswell_config"
 	TYPE="agni intel haswell"
-elif [ $choice -eq 6 ]; then
+elif [ $choice -eq 4 ]; then
 	CONFIG="agni_alderlake_config"
 	TYPE="agni intel alderlake"
 elif [ $choice -eq 0 ]; then
